4b0baa705e9d83047009178a69d9c53886c33fb8
i-Faith <faithcheptoo16@gmail.com>
2025-05-05 10:42:58 +0300

    Week 7 Assignment

---
+++ answers.sql
@@ -0,0 +1,77 @@
+--Original Table Name:productdetail Database Name:faith
+use faith;
+CREATE TABLE productdetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(255)
+);
+
+INSERT INTO productdetail VALUES
+(101, 'John Doe', 'Laptop, Mouse'),
+(102, 'Jane Smith', 'Tablet, Keyboard, Mouse'),
+(103, 'Emily Clark', 'Phone');
+
+--Question One
+--Viewing the data in the table productdetail
+select * from productdetail;
+
+--then query to manually transform the data into 1NF
+SELECT 101 AS OrderID, 'John Doe' AS CustomerName, 'Laptop' AS Product
+UNION ALL
+SELECT 101, 'John Doe', 'Mouse'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Tablet'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Keyboard'
+UNION ALL
+SELECT 102, 'Jane Smith', 'Mouse'
+UNION ALL
+SELECT 103, 'Emily Clark', 'Phone';
+
+--Question Two 
+--Initial table orderdetails
+use faith;
+CREATE TABLE orderdetails (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Product VARCHAR(100),
+    Quantity INT
+);
+
+INSERT INTO orderdetails VALUES
+(101, 'John Doe', 'Laptop', 2),
+(101, 'John Doe', 'Mouse', 1),
+(102, 'Jane Smith', 'Tablet', 3),
+(102, 'Jane Smith', 'Keyboard', 1),
+(102, 'Jane Smith', 'Mouse', 2),
+(103, 'Emily Clark', 'Phone', 1);
+
+--Creating Normalized tables (2NF)
+-- Orders table--
+CREATE TABLE Orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+
+-- OrderItems table--
+CREATE TABLE OrderItems (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    FOREIGN KEY (OrderID) REFERENCES Orders(OrderID)
+);
+
+-- Insert orders
+INSERT INTO Orders (OrderID, CustomerName)
+SELECT OrderID, CustomerName
+FROM OrderDetails;
+
+-- Insert order items
+INSERT INTO OrderItems (OrderID, Product, Quantity)
+SELECT OrderID, Product, Quantity
+FROM OrderDetails;
+
+--
+
+
+
